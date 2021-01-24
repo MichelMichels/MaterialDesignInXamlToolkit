eac378ffab292995d95f3aef7bf02efcb6efaeb6
Michel Michels <michel.michels.jr@gmail.com>
2021-01-24 11:59:30 +0100

    LeadingIcon on Button WIP

--- MaterialDesignThemes.Wpf/ButtonAssist.cs
+++ MaterialDesignThemes.Wpf/ButtonAssist.cs
@@ -5,6 +5,9 @@ namespace MaterialDesignThemes.Wpf
     public static class ButtonAssist
     {
         private static readonly CornerRadius DefaultCornerRadius = new CornerRadius(2.0);
+        private static readonly bool DefaultHasLeadingIcon = false;
+        private static readonly PackIconKind DefaultLeadingIcon = PackIconKind.Null;
+        
 
         #region AttachedProperty : CornerRadiusProperty
         /// <summary>
@@ -15,6 +18,22 @@ namespace MaterialDesignThemes.Wpf
 
         public static CornerRadius GetCornerRadius(DependencyObject element) => (CornerRadius)element.GetValue(CornerRadiusProperty);
         public static void SetCornerRadius(DependencyObject element, CornerRadius value) => element.SetValue(CornerRadiusProperty, value);
+        #endregion
+
+        #region AttachedProperty : HasLeadingIconProperty
+        public static readonly DependencyProperty HasLeadingIconProperty
+            = DependencyProperty.RegisterAttached("HasLeadingIcon", typeof(bool), typeof(ButtonAssist), new PropertyMetadata(DefaultHasLeadingIcon));
+
+        public static bool GetHasLeadingIcon(DependencyObject element) => (bool)element.GetValue(HasLeadingIconProperty);
+        public static void SetHasLeadingIcon(DependencyObject element, bool value) => element.SetValue(HasLeadingIconProperty, value);
+        #endregion
+
+        #region AttachedProperty : LeadingIconProperty
+        public static readonly DependencyProperty LeadingIconProperty
+            = DependencyProperty.RegisterAttached("LeadingIcon", typeof(PackIconKind), typeof(ButtonAssist), new PropertyMetadata(DefaultLeadingIcon));
+
+        public static PackIconKind GetLeadingIcon(DependencyObject element) => (PackIconKind)element.GetValue(LeadingIconProperty);
+        public static void SetLeadingIcon(DependencyObject element, PackIconKind value) => element.SetValue(LeadingIconProperty, value);
         #endregion
     }
 }
